a93c89f00c6ed4060cca1755d2e0220674853f8c
wenrongchen <43869196+wenronchen@users.noreply.github.com>
2020-06-21 19:49:31 -0400

    replace awk command

--- awk.py
+++ awk.py
@@ -10,16 +10,19 @@
 import pandas as pd 
 import sys
 
-df=pd.read_csv(sys.argv[1],sep='\t',header=None)
+strings=sys.argv[1].split(',')
+
+df=pd.read_csv(sys.argv[2],sep='\t',header=None)
 
 drop_index=[]
 for i in range(0,df.shape[0]):
     tmp=df.iloc[i][2]
-    if((tmp.find('KI270')!=-1) or (tmp.find('GL000')!=-1) or (tmp.find('_alt')!=-1)):
-        drop_index.append(i)
-        print(i)
+    for j in strings:
+        if(tmp.find(j)!=-1):
+            drop_index.append(i)
+            print(i)
     
 
 df=df.drop(df.index[drop_index])
 
-df.to_csv(sys.argv[2],sep='\t',header=None,index=0)
+df.to_csv(sys.argv[3],sep='\t',header=None,index=0)
